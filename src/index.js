ca092dc4c58f571783615ec116ba53c5b1cc2be7
wallaC-137 <wallace_031@hotmail.com>
2023-04-14 16:58:18 -0300

    fix: cria endpoit post: /login

--- src/index.js
+++ src/index.js
@@ -28,12 +28,11 @@ const firstFileFound = async (idSearch) => {
 
 const tokenGenerator = () => {
   const tokenLength = 16;
-  const possibleChars =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const possibleChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let token = '';
   for (let i = 0; i < tokenLength; i += 1) {
     token += possibleChars.charAt(
-      Math.floor(Math.random() * possibleChars.length)
+      Math.floor(Math.random() * possibleChars.length),
     );
   }
   return token;
