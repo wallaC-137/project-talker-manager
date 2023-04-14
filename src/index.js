6f42efb34e4e69fb4463d58d72c58df997d0ffef
wallaC-137 <wallace_031@hotmail.com>
2023-04-14 15:30:06 -0300

    cria o endpoit get: /talker

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(express.json());
@@ -6,9 +8,24 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
 
+const pathTalker = path.resolve(__dirname, 'talker.json');
+
+const fileReader = async () => {
+  try {
+    const test = await fs.readFile(pathTalker, 'utf-8');
+    console.log(JSON.parse(test));
+    return JSON.parse(test);
+  } catch (error) {}
+};
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  const data = await fileReader();
+  res.status(200).json(data || []);
 });
 
 app.listen(PORT, () => {
